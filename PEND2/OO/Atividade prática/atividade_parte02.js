f9f3394a43bac26c102a329e6c94abbee9cf3d4c
Bianca Bini <bianca.bini@aluno.senai.br>
2026-08-14 11:58:12 -0300

    Aula 14/08

---
+++ PEND2/OO/Atividade prática/atividade_parte02.js
@@ -0,0 +1,67 @@
+class Produto {
+
+    constructor(nome, preco, categoria, desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.desconto = desconto;
+    }
+
+    aplicarDesconto() {
+        const precoComDesconto = this.preco - (this.preco * (this.desconto / 100));
+        return precoComDesconto;
+    }
+
+    exibir() {
+        console.log(`${this.nome} custa R$ ${this.aplicarDesconto().toFixed(2)} com desconto aplicado.`);
+    }
+}
+
+class Estoque {
+
+    constructor() {
+        this.produtos = [];
+    }
+
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+    }
+
+    exibirNaTela() {
+
+        const resultado = document.querySelector("#resultado");
+
+        resultado.innerHTML = "";
+
+        this.produtos.forEach(produto => {
+
+            const precoFinal = produto.aplicarDesconto();
+
+            resultado.innerHTML += `
+            <div>
+                <p>Nome: ${produto.nome}</p>
+                <p>Categoria: ${produto.categoria}</p>
+                <p>Desconto: ${produto.desconto}%</p>
+                <p>Preço com desconto: R$ ${precoFinal.toFixed(2)}</p>
+            </div>
+        `;
+
+        });
+    }
+}
+
+//Array ou vetor para armazenar os produtos cadastrados
+const estoque = new Estoque();
+const nome = document.querySelector("#nome");
+const preco = document.querySelector("#preco");
+const categoria = document.querySelector("#categoria");
+const desconto = document.querySelector("#desconto");
+const botaoCadastrar = document.querySelector("#botaoCadastrar");
+
+botaoCadastrar.addEventListener("click", function () {
+
+    const produto = new Produto(nome.value, parseFloat(preco.value), categoria.value, parseFloat(desconto.value));
+
+    estoque.adicionarProduto(produto);
+    estoque.exibirNaTela();
+    });
